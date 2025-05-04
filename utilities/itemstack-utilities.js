c177d368a987a2dcadba476d8ae4a860aae30340
finnafinest_ <jitupersonalacc@gmail.com>
2025-05-04 03:36:08 -0700

    V2 Update: Added, Fixed, Organized static methods!

--- utilities/itemstack-utilities.js
+++ utilities/itemstack-utilities.js
@@ -1,6 +1,151 @@
-import { world, BlockPermutation, ItemStack, EquipmentSlot, system } from "@minecraft/server";
+import { world, BlockPermutation, ItemStack, EquipmentSlot, system, EnchantmentType } from "@minecraft/server";
 
 export class ItemStackUtils {
+
+    /**
+     * Copies all relevant data from one itemStack to another.
+     * @param {ItemStack} item The source item
+     * @param {ItemStack} nextItem The item to copy info into
+     * @returns {ItemStack} The modified item
+     */
+    static cloneItemStackInfo(item, nextItem) {
+        nextItem.nameTag = item.nameTag;
+        nextItem.amount = item.amount;
+        nextItem.keepOnDeath = item.keepOnDeath;
+        nextItem.lockMode = item.lockMode;
+        nextItem.setCanDestroy(item?.getCanDestroy());
+        nextItem.setLore(item?.getLore());
+        const durability1 = item?.getComponent('durability');
+        const durability2 = nextItem?.getComponent('durability');
+        durability2.damage = durability1.damage;
+        ItemStackUtils.applyEnchantments(nextItem, ItemStackUtils.getEnchantments(item));
+
+        return nextItem;
+    }
+
+    /**
+     * Creates a custom ItemStack with additional data (nameTag, lore, enchantments, and durability)
+     *
+     * @param {string} itemId - The typeId of item.
+     * @param {number} [amount=1] - The amount of item.
+     * @param {object} [data={}] - Optional data for the item.
+     * @param {string} [data.nameTag] - nameTag for the item.
+     * @param {string[]} [data.lore] - An array of Lore for the item.
+     * @param {Object.<string, number>} [data.enchantments] - An object of enchantment to apply. Example: { "minecraft:sharpness": 3 }.
+     * @param {number} [data.durabilityDamage] - The amount of durability damage to apply.
+     *
+     * @returns {ItemStack} The ItemStack with the applied properties.
+     *
+     * @example
+     * const sword = createItemStack("minecraft:diamond_sword", 1, {
+     *   nameTag: "Epic Sword",
+     *   lore: ["Sharp and shiny", "Level 3"],
+     *   enchantments: { "minecraft:sharpness": 3, "minecraft:unbreaking": 2 },
+     *   durability: 10
+     * });
+     */
+    static createItemStack(itemId, amount = 1, data = {}) {
+        const itemStack = new ItemStack(itemId, amount);
+
+        if (data.nameTag) itemStack.nameTag = data.nameTag;
+        if (data.lore) itemStack.setLore(data.lore);
+        if (data.enchantments) {
+            ItemStackUtils.applyEnchantments(itemStack, data.enchantments);
+        }
+        if (data.durabilityDamage) {
+            itemStack.getComponent('minecraft:durability').damage = data.durabilityDamage;
+        }
+        return itemStack;
+    }
+
+    /**
+     * Removes all enchantments from an item
+     * 
+     * @author finnafinest_
+     * @param {ItemStack} itemStack
+     * @throws {Error} If the item has no enchantments or enchantments cannot be modified.
+     */
+    static clearAllEnchantments(itemStack) {
+        const enchantableComp = itemStack.getComponent('minecraft:enchantable');
+        if (!enchantableComp.canAddEnchantment || enchantableComp.getEnchantments().length === 0) {
+            throw new Error("The item has no enchantments!");
+        }
+        if (!enchantableComp) return;
+        enchantableComp.removeAllEnchantments();
+    }
+
+    /**
+     * Serializes an item stack for storage
+     * @param {ItemStack} item
+     * @returns {Object}
+     */
+    static serializeItemStack(item) {
+        return {
+            typeId: item.typeId,
+            amount: item.amount,
+            nameTag: item.nameTag,
+            lore: item.getLore(),
+            durability: item.getComponent('minecraft:durability')?.damage,
+            enchantments: ItemStackUtils.getEnchantments(item)
+        };
+    }
+
+    /**
+     * Deserializes an item stack from storage data
+     * @param {Object} data
+     * @returns {ItemStack}
+     */
+    static deserializeItemStack(data) {
+        const itemStack = new ItemStack(data.typeId, data.amount);
+
+        if (data.nameTag) itemStack.nameTag = data.nameTag;
+        if (data.lore) itemStack.setLore(data.lore);
+        if (data.durability) {
+            itemStack.getComponent('minecraft:durability').damage = data.durability;
+        }
+        if (data.enchantments) {
+            ItemStackUtils.applyEnchantments(itemStack, data.enchantments);
+        }
+
+        return itemStack;
+    }
+
+    /**
+     * Extracts enchantments from an item
+     *
+     * @author finnafinest_
+     * @param {ItemStack} itemStack - The item to get enchantments from.
+     * @returns {Object.<string, number>} An object with enchantment ID and thier levels.
+     */
+    static getEnchantments(itemStack) {
+        const enchantableComp = itemStack.getComponent("minecraft:enchantable");
+        if (!enchantableComp) return {};
+
+        /** @type {Enchantment[]} */
+        const enchants = enchantableComp.getEnchantments();
+        const enchantsObj = {};
+        for (const enchant of enchants) {
+            enchantsObj[enchant.type] = enchant.level;
+        }
+        return enchantsObj;
+    }
+
+
+    /**
+     * 
+     * @param {ItemStack} itemStack 
+     * @param {Object} enchantments
+     */
+    static applyEnchantments(itemStack, enchantments) {
+        const enchantableComp = itemStack.getComponent("minecraft:enchantable");
+        if (!enchantableComp.canAddEnchantment) {
+            throw new Error("The item is not enchantable!");
+        }
+        if (!enchantableComp) return;
+        Object.entries(enchantments).forEach(([id, level]) => {
+            enchantableComp.addEnchantment({ type: new EnchantmentType(id), level: level });
+        });
+    }
 
     /**
       * Transfer enchantments from an item to another
@@ -55,7 +200,7 @@ export class ItemStackUtils {
         try {
             BlockPermutation.resolve(itemStack.typeId)
             return true;
-        } catch {}
+        } catch { }
         return false
     }
 
@@ -78,18 +223,18 @@ export class ItemStackUtils {
         //sacrifice random player >:D
         const rPlayer = world.getPlayers()[0];
         const equippable = rPlayer.getComponent("equippable");
-        const slots = ["Head", "Chest", "Legs", "Feet","Offhand","Mainhand"];
-        const prevItemStack = slots.map(s=>equippable.getEquipment(s));
+        const slots = ["Head", "Chest", "Legs", "Feet", "Offhand", "Mainhand"];
+        const prevItemStack = slots.map(s => equippable.getEquipment(s));
         let slot;
         for (const s of slots)
-        if (equippable.setEquipment(s,itemStack)) {
-            slot = s;
-            break;
-        }
-        prevItemStack.forEach((e,i)=>{
-            equippable.setEquipment(slots[i],e)
+            if (equippable.setEquipment(s, itemStack)) {
+                slot = s;
+                break;
+            }
+        prevItemStack.forEach((e, i) => {
+            equippable.setEquipment(slots[i], e)
         })
-        system.waitTicks(2).then(()=>{
+        system.waitTicks(2).then(() => {
             [
                 "armor.equip_chain",
                 "armor.equip_diamond",
@@ -98,7 +243,7 @@ export class ItemStackUtils {
                 "armor.equip_iron",
                 "armor.equip_leather",
                 "armor.equip_netherite"
-            ].forEach(sound=>rPlayer.runCommand(`stopsound @s ${sound}`))
+            ].forEach(sound => rPlayer.runCommand(`stopsound @s ${sound}`))
         })
         return slot
     }
@@ -123,7 +268,7 @@ export class ItemStackUtils {
       * })
       */
     static getStoredItems(itemStack) {
-        const {dimension,location} = world.getPlayers()[0];
+        const { dimension, location } = world.getPlayers()[0];
         const height = dimension.heightRange;
         const spawnLocation = {
             ...location,
@@ -132,27 +277,27 @@ export class ItemStackUtils {
 
         //Exclude existing item entity & Safeguard againts any hopper-like entities
         const excludeEntity = dimension.getEntitiesAtBlockLocation(spawnLocation)
-        excludeEntity.forEach(e=>{
+        excludeEntity.forEach(e => {
             e.initialPos = e.location;
             e.teleport({
                 ...e.location,
                 y: e.location.y + 5
             })
         })
-        dimension.spawnItem(itemStack,spawnLocation).applyDamage(5,{cause:"lava"})
-        excludeEntity.forEach(e=>{
+        dimension.spawnItem(itemStack, spawnLocation).applyDamage(5, { cause: "lava" })
+        excludeEntity.forEach(e => {
             e.teleport(e.initialPos)
         })
-        return dimension.getEntitiesAtBlockLocation(spawnLocation).filter(e=>{
+        return dimension.getEntitiesAtBlockLocation(spawnLocation).filter(e => {
             if (e.initialPos) {
                 delete e.initialPos
                 return;
             } else return true;
         })
-        .map((e,slot)=>{
-            const itemStack = e.getComponent("item").itemStack;
-            e.remove()
-            return itemStack
-        })
+            .map((e, slot) => {
+                const itemStack = e.getComponent("item").itemStack;
+                e.remove()
+                return itemStack
+            })
     }
 }
