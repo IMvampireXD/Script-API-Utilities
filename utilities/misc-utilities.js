5c5a7d5aec7efb7b7146abe28f51f56329faacb8
finnafinest_ <jitupersonalacc@gmail.com>
2025-05-04 03:39:25 -0700

    V2 Update: Added, Fixed, Organized static methods!

--- utilities/misc-utilities.js
+++ utilities/misc-utilities.js
@@ -1,3 +1,28 @@
+
+/**
+ * Returns true every tick as of the specified tick
+ * @param {number} interval The interval in ticks (1 tick = 20 sec)
+ * @returns {boolean}
+ */
+export const ticks = (interval) => system.currentTick % interval === 0;
+
+/**
+ * Finds first and last index of a substring in a string.
+ * @param {string} s The string to search within
+ * @param {string} i The substring to search for
+ * @returns {{ l: number, f: number }} Last and first index
+ */
+export const indexOf = (s, i) => ({ l: s.lastIndexOf(i), f: s.indexOf(i) });
+
+/**
+ * Finds a line in item lore that contains a specific string.
+ * @param {string} s The string to search for
+ * @param {string[]} l The lore array
+ * @returns {string | undefined} The matched lore line
+ */
+export const findLore = (s, l) => l.find((f) => f.includes(s));
+
+
 export class MiscUtils {
 
   /**
@@ -10,6 +35,36 @@ export class MiscUtils {
     */
   static renameFn(fn, name) {
     return Object.defineProperty(fn, "name", { value: name })
+  }
+
+  /**
+   * Finds keys in an object where the value contains the given string.
+   * @param {string} string The string to search for
+   * @param {Object.<string, string[]>} array The object to search in
+   * @returns {string[]} Matching keys
+   */
+  static getKey(string, array) {
+    return Object.keys(array).filter((s) => array[s].includes(string));
+  }
+
+  /**
+  * Delays execution for a number of game ticks.
+  * @param {number} ticks Number of ticks to wait (1 tick = 20 sec)
+  * @returns {Promise<void>}
+  */
+  static sleep(ticks) {
+    return new Promise((resolve) => system.runTimeout(resolve, ticks));
+  }
+
+  /**
+  * Repeats a callback a specified number of times.
+  * @param {() => void} callback The function to run
+  * @param {number} times Number of times to run the function
+  */
+  static repeat(callback, times) {
+    for (let i = 0; i < times; i++) {
+      callback();
+    }
   }
 
 }
