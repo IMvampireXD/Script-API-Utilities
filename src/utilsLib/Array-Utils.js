92d9ed4466941300d9ea76ef8bf48d9d6f27cb5f
Keyyard <keyyard8888@gmail.com>
2025-06-18 11:55:29 +0700

    feat(): add CONTRIBUTING fix: more detailed descriptions for some functions

--- src/utilsLib/Array-Utils.js
+++ src/utilsLib/Array-Utils.js
@@ -1,7 +1,12 @@
 class ArrayUtils {
+	
 	/**
-	 * Randomly shuffle the elements of an array.
+	 * Shuffles an array using the Fisher-Yates algorithm.
+	 * 
+	 * @param {Array} array - The array to shuffle.
+	 * @returns {Array} A new array with elements shuffled randomly.
 	 */
+
 	static shuffleArray(array) {
 		const result = [...array];
 		for (let i = result.length - 1; i > 0; i--) {
