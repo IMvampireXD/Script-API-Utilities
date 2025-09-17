bb7a24c5093b70a89db4e2c761a731c93a484c00
finnafinest_ <jitupersonalacc@gmail.com>
2025-09-17 21:38:45 +0600

    Create TimeOfDay.js

---
+++ src/utilsLib/TimeOfDay.js
@@ -0,0 +1,8 @@
+export default class TimeOfDay {
+    static Day = 1000
+    static Midnight = 18000
+    static Night = 13000
+    static Noon = 6000
+    static Sunrise = 23000
+    static Sunset = 12000
+}
