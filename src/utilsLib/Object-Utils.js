a5849205a3ce3a403cb58eff7f767c75408d76c4
finnafinest_ <jitupersonalacc@gmail.com>
2025-09-19 17:20:38 +0600

    added simplex/perlin Noise and added some object utils

--- src/utilsLib/Object-Utils.js
+++ src/utilsLib/Object-Utils.js
@@ -1,4 +1,5 @@
 export class ObjectUtils {
+
 	/**
 	 * Creates a deep clone of an object using JSON serialization
 	 * allowing not to edit original object when modifying the deep cloned object
@@ -6,11 +7,47 @@ export class ObjectUtils {
 	static deepClone(obj) {
 		return JSON.parse(JSON.stringify(obj));
 	}
+
 	/**
 	 * Returns boolean whether the value is a plain object or not.
 	 */
 	static isObject(value) {
 		return typeof value === 'object' && value !== null && !Array.isArray(value);
+	}
+
+	/**
+	 * @author ConMaster2112
+	 * 
+	 * Useful for dynamically enhancing objects and prototypes,
+	 * allowing flexible inheritance and method overriding.
+	 */
+	static OverTakesJS(prototype, object) {
+		const clone = Object.create(Object.getPrototypeOf(prototype), Object.getOwnPropertyDescriptors(prototype));
+		Object.setPrototypeOf(object, clone);
+		Object.defineProperties(prototype, Object.getOwnPropertyDescriptors(object));
+		return clone;
+	}
+
+	/**
+	 * @author frostice482
+	 *
+	 * Renames function name
+	 * @param fn Function
+	 * @param name New function name
+	 * @returns function
+	 */
+	static renameFn(fn, name) {
+		return Object.defineProperty(fn, "name", { value: name });
+	}
+
+	/**
+	 * Finds keys in an object where the value contains the given string.
+	 * @param {string} string The string to search for
+	 * @param {Object.<string, string[]>} obj The object to search in
+	 * @returns {string[]} Matching keys
+	 */
+	static getKey(string, obj) {
+		return Object.keys(obj).filter((s) => obj[s].includes(string));
 	}
 
 }
